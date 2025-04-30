3c0df1d2602b4817de80ed4b1cbe2fecc331c55b
Ana Beatriz Sousa <126481735+beatrizbia000@users.noreply.github.com>
2025-04-30 19:45:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+
+app.get("/",function(req,res){
+    res.send(`
+    <html>
+      <head><title>Início</title></head>
+      <body>
+        <h1>Bem-vindo à Página Inicial!</h1>
+        <p>Esta é a rota /</p>
+      </body>
+    </html>
+  `)
+});
+
+
+app.get("/Sobre",function(req,res){
+    res.send(`
+    <html>
+      <head><title>Sobre</title></head>
+      <body>
+        <h1>Sobre Nós</h1>
+        <p>Esta é a rota /sobre</p>
+      </body>
+    </html>
+  `)
+});
+
+app.get("/Contato",function(req,res){
+    res.send(`
+    <html>
+      <head><title>Contato</title></head>
+      <body>
+        <h1>Contato</h1>
+        <p>Esta é a rota /contato</p>
+      </body>
+    </html>
+  `)
+
+});
+
+app.listen(4000,function(erro){
+    if(erro){
+        console.log("Erro ao iniciar");
+    }else{
+        console.log("Servidor iniciado");
+    }
+});
