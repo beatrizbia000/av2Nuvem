ac8a35eaf6a0ab9f73c12b0ee4e3f4f1754bf12e
Ana Beatriz Sousa <126481735+beatrizbia000@users.noreply.github.com>
2025-05-14 20:11:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,115 @@
-const express = require("express");
+//import express from 'express';
+const express = require('express');
+
+//import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+//import {createClient} from '@supabase/supabase-js'
+const supabaseClient = require('@supabase/supabase-js');
+
+//import morgan from 'morgan';
+const morgan = require('morgan');
+
+//import bodyParser from "body-parser";
+const bodyParser = require('body-parser');
+
+//import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js";
+
 const app = express();
 
-app.get("/",function(req,res){
-    res.send(`
-    <html>
-      <head><title>Início</title></head>
-      <body>
-        <h1>Bem-vindo à Página Inicial!</h1>
-        <p>Esta é a rota /</p>
-      </body>
-    </html>
-  `)
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+
+
+// using morgan for logs
+app.use(morgan('combined'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const supabase = 
+    supabaseClient.createClient('https://oduobfbgysmiksjwgpmk.supabase.co', 
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9kdW9iZmJneXNtaWtzandncG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwODkzMzAsImV4cCI6MjA2MjY2NTMzMH0.X3Fk_6SyVo5xFYK9T8KCwEXdBU8JC7fMLiltGL4yZBI')
+
+
+app.get('/products', async (req, res) => {
+    const {data, error} = await supabase
+        .from('products')
+        .select()
+    res.send(data);
+    console.log(`lists all products${data}`);
 });
 
+app.get('/products/:id', async (req, res) => {
+    console.log("id = " + req.params.id);
+    const {data, error} = await supabase
+        .from('products')
+        .select()
+        .eq('id', req.params.id)
+    res.send(data);
 
-app.get("/Sobre",function(req,res){
-    res.send(`
-    <html>
-      <head><title>Sobre</title></head>
-      <body>
-        <h1>Sobre Nós</h1>
-        <p>Esta é a rota /sobre</p>
-      </body>
-    </html>
-  `)
+    console.log("retorno "+ data);
 });
 
-app.get("/Produtos",function(req,res){
-    res.send(`
-    <html>
-      <head><title>Contato</title></head>
-      <body>
-        <h1>Contato</h1>
-        <p>Esta é a rota /contato</p>
-      </body>
-    </html>
-  `)
-
-});
-
-app.listen(4000,function(erro){
-    if(erro){
-        console.log("Erro ao iniciar");
-    }else{
-        console.log("Servidor iniciado");
+app.post('/products', async (req, res) => {
+    const {error} = await supabase
+        .from('products')
+        .insert({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+        })
+    if (error) {
+        res.send(error);
     }
+    res.send("created!!");
+    console.log("retorno "+ req.body.name);
+    console.log("retorno "+ req.body.description);
+    console.log("retorno "+ req.body.price);
+
+});
+
+app.put('/products/:id', async (req, res) => {
+    const {error} = await supabase
+        .from('products')
+        .update({
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price
+        })
+        .eq('id', req.params.id)
+    if (error) {
+        res.send(error);
+    }
+    res.send("updated!!");
+});
+
+app.delete('/products/:id', async (req, res) => {
+    console.log("delete: " + req.params.id);
+    const {error} = await supabase
+        .from('products')
+        .delete()
+        .eq('id', req.params.id)
+    if (error) {
+        res.send(error);
+    }
+    res.send("deleted!!")
+    console.log("delete: " + req.params.id);
+
+});
+
+app.get('/', (req, res) => {
+    res.send("Hello I am working my friend Supabase <3");
+});
+
+//app.get('*', (req, res) => {
+//  res.send("Hello again I am working my friend to the moon and behind <3");
+//});
+
+app.listen(3000, () => {
+    console.log(`> Ready on `);
 });
